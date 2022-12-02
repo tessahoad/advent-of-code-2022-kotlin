6ab6f446b226fc4763b29c820d8a8705eacbaba1
Tessa Hoad <contact@tessahoad.co.uk>
2022-12-02 12:14:29 +0000

    AoC 2022: Day Two, Part Two

--- src/main/kotlin/daytwo/App.kt
+++ src/main/kotlin/daytwo/App.kt
@@ -7,20 +7,36 @@ object App {
     fun main(args: Array<String>) {
         val fileContent = App::class.java.getResource("/daytwo/input.txt").readText()
 
+        // Part One
         val myShapes = fileContent.split("\n").map { newline ->
             val (_, xyzMapping) = newline.split(" ")
             Shape.fromXYZCHar(xyzMapping.toCharArray().first())
         }
 
-        val results = fileContent.split("\n").map { newline ->
-            val (abcMapping, xyzMapping) = newline.split(" ")
-            val elfShape = Shape.fromABCCHar(abcMapping.toCharArray().first())
-            val myShape = Shape.fromXYZCHar(xyzMapping.toCharArray().first())
-            myShape.beats(elfShape)
+        // Part Two
+        val myResults = fileContent.split("\n").map { newline ->
+            val (_, xyzMapping) = newline.split(" ")
+            Result.fromXYZCHar(xyzMapping.toCharArray().first())
         }
 
-        val score = results.map { result -> result.score }.sum() + myShapes.map { shape -> shape.score }.sum()
+        val elfShapes = fileContent.split("\n").map { newline ->
+            val (abcMapping, _) = newline.split(" ")
+            Shape.fromABCCHar(abcMapping.toCharArray().first())
+        }
 
-        println("Part One answer: $score")
+        println("Part One answer: ${partOneScore(elfShapes, myShapes)}")
+        println("Part Two answer: ${partTwoScore(elfShapes, myResults)}")
+    }
+
+    private fun partOneScore(elfShapes: List<Shape>, myShapes: List<Shape>): Int {
+        val results = myShapes.zip(elfShapes).map { (myShape, elfShape) -> myShape.beats(elfShape) }
+        return results.map { result -> result.score }.sum() + myShapes.map { shape -> shape.score }.sum()
+    }
+
+    private fun partTwoScore(elfShapes: List<Shape>, myResults: List<Result>): Int {
+        val myShapes = elfShapes.zip(myResults).map { (elfShape, myResult) ->
+            Shape.values().first { shape -> shape.beats(elfShape) == myResult }
+        }
+        return myResults.map { result -> result.score }.sum() + myShapes.map { shape -> shape.score }.sum()
     }
 }
--- src/main/kotlin/daytwo/Result.kt
+++ src/main/kotlin/daytwo/Result.kt
@@ -1,7 +1,13 @@
 package daytwo
 
-enum class Result(val score: Int) {
-    WIN(6),
-    DRAW(3),
-    LOSS(0)
+enum class Result(val score: Int, val xyzMapping: Char) {
+    WIN(6, 'Z'),
+    DRAW(3, 'Y'),
+    LOSS(0, 'X');
+
+    companion object {
+        fun fromXYZCHar(char: Char) : Result {
+            return Result.values().first { shape -> shape.xyzMapping == char }
+        }
+    }
 }
