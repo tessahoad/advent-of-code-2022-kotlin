720b13747183a5c9872465cfc5c76d0685342b40
Tessa Hoad <contact@tessahoad.co.uk>
2022-12-06 20:33:07 +0000

    AoC 2022: Day Six, Part Two

--- src/main/kotlin/daysix/App.kt
+++ src/main/kotlin/daysix/App.kt
@@ -5,10 +5,13 @@ object App {
     fun main(args: Array<String>) {
         val signal = App::class.java.getResource("/daysix/input.txt").readText()
 
-        val windows = signal.windowed(4).map{ it.toSet() }
-        val startOfPacketMarker = windows.first{ it.size == 4 }.joinToString("")
+        println("Part one answer: ${getNumberOfProcessedCharacters(signal, 4)}")
+        println("Part Two answer: ${getNumberOfProcessedCharacters(signal, 14)}")
+    }
 
-        val processedCharacters = signal.indexOf(startOfPacketMarker) + 4
-        println("Part one answer: $processedCharacters")
+    private fun getNumberOfProcessedCharacters(signal: String, windowSize: Int): Int {
+        val windows = signal.windowed(windowSize).map{ it.toSet() }
+        val startOfPacketMarker = windows.first{ it.size == windowSize }.joinToString("")
+        return signal.indexOf(startOfPacketMarker) + windowSize
     }
 }
